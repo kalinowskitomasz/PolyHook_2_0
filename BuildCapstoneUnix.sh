39959c7876514936f179c9d1f88ec9b973201340
tk <tomasz.kalinow@gmail.com>
2018-10-17 08:23:11 +1100

    Disable 32 bit build for mac

--- BuildCapstoneUnix.sh
+++ BuildCapstoneUnix.sh
@@ -1,12 +1,14 @@
 cd capstone
-mkdir build32
+if [ "$(uname)" == "Darwin" ]; then # Don't build 32 bit version on Mac as it is deprecated
+  mkdir build32
+  cd build32
+  cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_BUILD_TYPE=Release ../
+  make
+  cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DEBUG_POSTFIX=_debug ../
+  make
+  cd ../
+fi
 mkdir build64
-cd build32
-cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_BUILD_TYPE=Release ../
-make
-cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_BUILD_TYPE=Debug -DCMAKE_DEBUG_POSTFIX=_debug ../
-make
-cd ../
 cd build64
 cmake -DCMAKE_BUILD_TYPE=Release ../
 make
